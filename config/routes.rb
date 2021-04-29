8adf0dbf94001b242c50f7ec1b2d67a749483b07
Ronak Vimadalal <ronakvimadalal@live.com>
2021-04-28 19:00:52 -0700

    added article listing page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :articles, only: [:show]
+  resources :articles , only: [:show, :index]
 
 end
