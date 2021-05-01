553a32a444ff0f375e46fc7c29ceb8a7f766b87a
Ronak Vimadalal <ronakvimadalal@live.com>
2021-05-01 15:38:21 -0700

    Create Read and Update actions added

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -5,6 +5,60 @@ class ArticlesController < ApplicationController
   end
 
   def index
+    # created variable so articles/new does not give Empty Class Error
     @articles = Article.all
   end
+
+  def new
+    @article  = Article.new()
+  end
+
+  def edit
+
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    # Whitelisted the title and description fields of Article to be filled from the form
+
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    #render plain: @article
+    #
+    # If article was successfully saved
+    if @article.save
+
+      # render plain: params[:article]
+      #
+      # We just created an article it would make sense to show the 'show' page
+      # redirected to show page
+      # path is given by: prefix(from routes)_path(@article - id of the article is stored here)
+      redirect_to article_path(@article)
+      # display a message using Rails' flash functionality
+      flash[:notice] = "Article was created successfully"
+    else
+      flash[:notice] = "Article was created unsuccessfully"
+      render 'articles/new'
+      end
+    # ^ Can also be written as redirect_to @article
+
+    # Can also be redirected to a URI like
+    # redirect_to '/articles'
+    # Or to a prefix_path
+    # redirect_to root_path
+    # For further information look up redirect_to documentation
+
+  end
+
+  def update
+
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article is updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :articles , only: [:show, :index]
+  # add routes here to see in expanded view
+  resources :articles , only: [:show, :index, :new, :create, :edit, :update]
 
 end
